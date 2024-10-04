af3ec455d6763a6b22f0f65fe81234ed8142ac28
Ryan Healy <healydatascience@gmail.com>
2024-10-04 11:45:29 -0400

    black formatting and fixed pylint errors

--- Team 4/src/clean_data.py
+++ Team 4/src/clean_data.py
@@ -2,42 +2,42 @@ import numpy as np
 import pandas as pd
 
 # make sure its in English
- # could use spaCy, want to avoid having to hit an api, or downloading too much
- # maybe have that as an integration test?
+# could use spaCy, want to avoid having to hit an api, or downloading too much
+# maybe have that as an integration test?
 
 
-def clean_bookend_quotes(string:str) -> str:
+def clean_bookend_quotes(string: str) -> str:
     """take out starting and ending quotes."""
 
     if string.startswith("'") and string.endswith("'"):
         string = string[-1:1]
-    
+
     elif string.startswith('"') and string.endswith('"'):
         string = string[-1:1]
 
     return string
 
 
-def clean_news(df:pd.DataFrame) -> pd.DataFrame:
+def clean_news(df: pd.DataFrame) -> pd.DataFrame:
     """Clean the News Series.
-    
+
     using strip, to strip the start and end of the string.
     # TODO: should also check encoding
     """
 
     strings_ = []
-    nan_cnt, byte_cnt = 0,0
+    nan_cnt, byte_cnt = 0, 0
 
     for news_string in df["News"].values:
 
         if news_string is np.nan:
             strings_.append("")
             nan_cnt += 1
-            continue 
-        
+            continue
+
         if news_string.startswith("b"):
             cleaned_string = clean_bookend_quotes(news_string[1:].strip())
-            strings_.append(cleaned_string) 
+            strings_.append(cleaned_string)
             byte_cnt += 1
 
         else:
@@ -47,4 +47,3 @@ def clean_news(df:pd.DataFrame) -> pd.DataFrame:
     print(f"of {df.shape}, byte prefixes {byte_cnt} - filled in nans {nan_cnt}")
     df = df.assign(News=strings_)
     return df
- 
