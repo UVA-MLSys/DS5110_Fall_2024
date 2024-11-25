5836794b2915038d9f1800bc51b956c51a0e9fd6
Ryan Healy <healydatascience@gmail.com>
2024-11-25 17:36:44 -0500

    adding in linter

---
+++ Team 4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cosmic_ai',
+    packages=["Team 4"]
+    package_dir={"Team 4": "src"}
+    version='0.0.1',
+    url='https://github.com/UVA-MLSys/DS5110_Fall_2024',
+    author='Ryan Healy',
+    author_email='rah5ff@virginia.edu',
+    description='Scaling Redshift Predictions', 
+)
